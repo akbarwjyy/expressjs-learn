90c9704ccaca873d6e013ce9f4893215dac53bf3
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2025-07-05 20:14:05 +0700

    initial commit

---
+++ jwt-auth-api/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+app.use(express.json());
+
+app.use("api/auth", authRoutes);
+
+// test protected route
+app.get("/api/profile", require("./middlewares/authMiddleware"), (req, res) => {
+  res.json({ message: `Selamat datang ${req.user.username}` });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server berjalan di http://localhost:${process.env.PORT}`);
+});
